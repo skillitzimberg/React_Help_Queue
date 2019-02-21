819116e3d42ac1e54ddffae3e1901001278963e2
Scott Bergler <commandinghands@gmail.com>
2019-02-21 06:57:54 -0800

    change selectedTicket prop type to string from object..

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -24,7 +24,7 @@ Admin.propTypes = {
   ticketList: PropTypes.array,
   currentRouterPath: PropTypes.string.isRequired,
   onTicketSelection: PropTypes.func.isRequired,
-  selectedTicket: PropTypes.object
+  selectedTicket: PropTypes.string
 };
 
 export default Admin;
